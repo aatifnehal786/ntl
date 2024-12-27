768e76b6238efbcc7aafb4b444ca0447ef9289ea
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2024-12-27 21:26:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ mongoose.connect(process.env.MONGO_URL)
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
 
 
 
