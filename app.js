86216d1a10ae9c0de63d132e9cbeb5408eb8bdd7
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2025-01-01 01:46:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cors({
     origin: "https://67744cbbeee13c8fed1785a4--chimerical-choux-4b6fa1.netlify.app/",
-    credentials: true,
+    credentials: false,
 }));
 
 app.use((req, res, next) => {
