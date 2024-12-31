de6ee90ac9faa51f16f13334726b0b75f7a6950b
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2025-01-01 00:37:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(cors({
-    origin: "https://your-frontend.onrender.com",
+    origin: "https://ntl.onrender.com",
     credentials: true,
 }));
 
