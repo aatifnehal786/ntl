a15382e021b84c67adbb29cd25ed5d0fe81ed059
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2025-01-01 00:36:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ mongoose.connect(process.env.MONGO_URL)
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(cors({
+    origin: "https://your-frontend.onrender.com",
+    credentials: true,
+}));
+
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
