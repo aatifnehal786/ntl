737e894d829eec2c14c97e279c78fbdd941c0bae
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2025-01-08 21:15:29 +0530

    Update verifyEmail.js

--- verifyEmail.js
+++ verifyEmail.js
@@ -1,1 +1,22 @@
+const userModel = require(./userModel)
+function verifiedEmail(req,res,next)
+{
 
+  const userEmail = req.body
+
+  const user = userModel.findOne({email:userEmail})
+
+  if(user.isVerified===false)
+  {
+    res.status(401).send({message:"Email not verified, Please verify Email"})
+    
+  }
+  else
+  {
+    next();
+    
+  }
+  
+}
+
+module.exports = verifiedEmail;
