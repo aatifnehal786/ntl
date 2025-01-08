220cac33de0bd72dd7c9136e534d8d45631f11c2
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2025-01-08 22:10:49 +0530

    Update userModel.js

--- userModel.js
+++ userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const crtpto = require('crypto')
+
 const userSchema = mongoose.Schema({
     name:{
         type:String,
@@ -18,19 +18,12 @@ const userSchema = mongoose.Schema({
         required:[true,"Age is Mandatory"],
         min: 12
     },
-   
-    otp: { type: String },
-    otpExpires: { type: Date },
-    isVerified: { type: Boolean, default: false }
+    isEmailVerified: { type: Boolean, default: false },
+
     
 },{timestamps:true})
 
-userSchema.methods.generateOtp = function () {
-    const otp = crypto.randomInt(100000, 999999).toString(); // Generate a 6-digit OTP
-    this.otp = otp;
-    this.otpExpires = Date.now() + 10 * 60 * 1000; // OTP valid for 10 minutes
-    return otp;
-};
+
 
 const userModel = mongoose.model("users",userSchema)
 
