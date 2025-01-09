716b323da533bfb48e9450dfd5a9b47881f4e56e
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2025-01-09 21:51:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -353,7 +353,7 @@ app.delete("/un-register",verifiedToken,async (req,res)=>{
 
     if(!userEmail)
     {
-        res.status(404).send({message:"User Not Found"})
+        return res.status(404).json({message:"User Not Found"})
     }
     
         try
@@ -362,14 +362,14 @@ app.delete("/un-register",verifiedToken,async (req,res)=>{
     
             if(user)
             {
-                res.status(200).send({message:"User Un-registered Successfully"})
+                return res.status(200).json({message:"User Un-registered Successfully"})
             }
     
         }
         catch(error)
         {
             console.log(error)
-            res.status(500).send({message:"An error occured while Un-registering User"})
+            return res.status(500).json({message:"An error occured while Un-registering User"})
         }
 
     
